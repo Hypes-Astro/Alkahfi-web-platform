f6c3dd4c5d80c97462305094d42fffd5b1355901
Hypes-Astro <muhamadalifanwar@gmail.com>
2024-04-18 14:28:57 +0700

    Fix Hadish

--- src/components/Home/CardServiceFeature.jsx
+++ src/components/Home/CardServiceFeature.jsx
@@ -4,7 +4,37 @@ import ScheduleImg from "../../assets/Calendar.png";
 import BookImg from "../../assets/AlQuranBook.png";
 // import DoaImg from "../../assets/Chat.png"
 
+import { useState, useEffect } from "react";
+import HadisController from "../../Controller/HadisController";
+
 const CardServiceFeature = () => {
+  const [randomHadis, setRamdomHadis] = useState(null);
+
+  useEffect(() => {
+    fetchHadis();
+  }, []);
+
+  const fetchHadis = async () => {
+    const dataHadis = new HadisController();
+    try {
+      const randomHadis = await dataHadis.getRandomHadis();
+      setRamdomHadis(randomHadis);
+      // const hadis = randomHadis ? randomHadis : "Loading..";
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleButtonClickHadis = () => {
+    fetchHadis();
+    console.log(randomHadis);
+    document.getElementById("my_modal_5").showModal();
+  };
+  const perawi = randomHadis ? randomHadis.info.perawi.name : "Loading";
+  const noHadis = randomHadis ? randomHadis.data.number : "Loading";
+  const arHadis = randomHadis ? randomHadis.data.arab : "Loading";
+  const idHadis = randomHadis ? randomHadis.data.id : "Loading";
+
   return (
     <div className="ContainerFeature w-full flex flex-col p-5 justify-center items-center flex-wrap">
       <h1 className="text-2xl font-bold mb-4">Service</h1>
@@ -27,7 +57,10 @@ const CardServiceFeature = () => {
           </div>
         </a>
 
-        <button className="shadow-sm hover:shadow-lg">
+        <button
+          className="shadow-sm hover:shadow-lg"
+          onClick={handleButtonClickHadis}
+        >
           <div className="card w-44 bg-blue-100 shadow-sm rounded-md flex items-center justify-center py-3">
             <img src={BookImg} alt="" className="object-cover w-36" />
             <h2 className="text-base bg-base-100 p-1 px-3 rounded-full text-center hover:bg-yellow-50">
@@ -39,11 +72,15 @@ const CardServiceFeature = () => {
       {/* Modal Hadis */}
       <dialog
         id="my_modal_5"
-        className="modal modal-bottom sm:modal-middle pets"
+        className="modal modal-bottom w-auto sm:modal-middle pets"
       >
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Nama hadis</h3>
-          <p className="py-4">Isi Hadis</p>
+          <div className="headHadis flex w-full  justify-between items-center">
+            <h3 className="font-bold text-lg">Riwayah {perawi}</h3>
+            <p>Nomor {noHadis} </p>
+          </div>
+          <p className="p-4"> {arHadis} </p>
+          <p className="py-4">{idHadis}</p>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in the form, it will close the modal */}
